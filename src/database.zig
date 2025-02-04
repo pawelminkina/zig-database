bf2a1fb1546b41e209fbb1a3f3a8542d3da7bb35
pavixonpl <pawmin14@gmail.com>
2025-02-04 23:12:38 +0100

    database file creation

--- src/database.zig
+++ src/database.zig
@@ -3,8 +3,54 @@ const std = @import("std");
 pub const CREATE_DATABASE_COMMAND = "CREATE DATABASE";
 
 pub fn CreateDatabase(command: []const u8) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
     const stdout = std.io.getStdOut().writer();
     const databaseCreationValues = std.mem.trim(u8, command[CREATE_DATABASE_COMMAND.len..command.len], " ");
+    try stdout.print("{s}\n", .{databaseCreationValues});
 
-    try stdout.print("{s}", .{databaseCreationValues});
+    const workingDirectoryPath = try std.fs.getAppDataDir(allocator, "zigdatabase");
+    defer allocator.free(workingDirectoryPath);
+
+    try stdout.print("{s}\n", .{workingDirectoryPath});
+
+    const dir = try GetOrCreateDirectory(workingDirectoryPath);
+
+    if (std.mem.containsAtLeast(u8, databaseCreationValues, 1, " ")) {
+        try stdout.print("Given database name has space inside, TODO fail the command", .{});
+        return;
+    }
+
+    var databaseFileNameArray = std.ArrayList(u8).init(allocator);
+    defer databaseFileNameArray.deinit();
+    try databaseFileNameArray.appendSlice(databaseCreationValues);
+    try databaseFileNameArray.appendSlice(".zigdatabasefile");
+    const databaseFileName = databaseFileNameArray.items;
+
+    const file = try dir.createFile(databaseFileName, .{ .read = true });
+    defer file.close();
+
+    try file.writeAll(databaseCreationValues);
+}
+
+fn GetOrCreateDirectory(path: []const u8) !std.fs.Dir {
+    const dir = std.fs.cwd().openDir(path, .{}) catch |err| {
+        if (err == error.FileNotFound) {
+            try std.fs.cwd().makeDir(path);
+            return try std.fs.cwd().openDir(path, .{});
+        } else {
+            return err;
+        }
+    };
+    return dir;
+}
+
+fn ConcatStrings(a: []const u8, b: []const u8, alloc: std.mem.Allocator) []const u8 {
+    var string = std.ArrayList(u8).init(alloc);
+    defer string.deinit();
+    try string.appendSlice(a);
+    try string.appendSlice(b);
+
+    return alloc.dupe(u8, string.items);
 }
